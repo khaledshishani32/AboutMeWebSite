e78c8159e85335e3170d2e3705aad99e5d5ab3c0
khaledshishani32 <khaled.shishani32@gmail.com>
2021-03-30 22:04:27 +0300

    added 6th and 7th questions to game

--- js/app.js
+++ js/app.js
@@ -1,115 +1,212 @@
 'use strict';
 
+
+let correctAnswer = 0;
+
 alert("Welcom in a funny websit 🤗 ");
-let userName=prompt("Please enter your name 😃 ");
-console.log("The clint name  is " + userName);
-alert('Welcome'+userName +' we will play a fun game like guessing questions let\s go 😃  ');
 
-const myName="khaled";
+let userName = prompt("Please enter your name 😃 ");
 
-let askForMyName=prompt("Do you think my name is john wick ? chooes Yes/y or No/n ");
-if(askForMyName.toLocaleLowerCase() === "yes" || askForMyName.toLowerCase() === "y"){  
+while (!userName) {
+
+
+    userName = prompt("Please enter your name ! 😁")
+
+}
+//console.log("The clint name  is " + userName);
+
+alert('Welcome ' + userName + ' we will play a fun game like guessing questions let\s go 😃  ');
+
+const myName = "khaled";
+
+let askForMyName = prompt("Do you think my name is john wick ? chooes Yes/y or No/n ");
+if (askForMyName.toLocaleLowerCase() === "yes" || askForMyName.toLowerCase() === "y") {
     alert(` ❌  No way john wick is an actor . My name is ${myName}!`);
     console.log(askForMyName.toLocaleLowerCase());
-}else if(askForMyName.toLocaleLowerCase() ==="no" || askForMyName.toLocaleLowerCase()==="n"){
+} else if (askForMyName.toLocaleLowerCase() === "no" || askForMyName.toLocaleLowerCase() === "n") {
     alert(` ✔️  You are amazing , my name is ${myName} !`);
-}else{
+    correctAnswer++;
+} else {
     alert("Please answer with only yes/y or no/n with no spaces");
 }
 
 const myAge = 27;
 
-let askForMyAge=prompt("Do you thing my age between 18 & 25  🤔 ?  chooes Yes or No ");
+let askForMyAge = prompt("Do you thing my age between 18 & 25  🤔 ?  chooes Yes or No ");
 
-askForMyAge=askForMyAge.toLocaleLowerCase();
+askForMyAge = askForMyAge.toLocaleLowerCase();
 
-switch(askForMyAge){
-    case  "yes" :
-    case   "y" :   
+switch (askForMyAge) {
+    case "yes":
+    case "y":
         alert(" ❌  No , I am 27 years old  😁 ");
-        console.log(`my age is ${myAge} !` );
+        console.log(`my age is ${myAge} !`);
         break;
-    
-    case  "no" :
-    case   "n" :  
-    alert(` ✔️  Yes, your answer is correct. I am ${myAge} years old  😁 `);
-    console.log(`my age is ${myAge} !` );
-    break;
-    default : 
-    alert("Please answer with only yes/y or no/n with no spaces");
+
+    case "no":
+    case "n":
+        alert(` ✔️  Yes, your answer is correct. I am ${myAge} years old  😁 `);
+        correctAnswer++;
+        console.log(`my age is ${myAge} !`);
+        break;
+    default:
+        alert("Please answer with only yes/y or no/n with no spaces");
 }
 
 const myFavActor = "john wick";
 
-let favActor=prompt("Do you think a john wick of my favourite actor\s ? 🤔 , Be smart 😁 chooes Yes or No ");
+let favActor = prompt("Do you think a john wick of my favourite actor\s ? 🤔 , Be smart 😁 chooes Yes or No ");
 
-favActor=favActor.toLocaleLowerCase();
+favActor = favActor.toLocaleLowerCase();
 
-switch(favActor){
-    case  "no" :
-    case   "n" :   
+switch (favActor) {
+    case "no":
+    case "n":
         alert(" ❌  No , Didn't you notice that I mentioned the john wick name previously ?? Was it a coincidence 🤔 ?? It's okay, let's continue 😃 ");
-        console.log(` ${myFavActor} is one of my favourite actor\s ` );
+        console.log(` ${myFavActor} is one of my favourite actor\s `);
         break;
-    
-    case  "yes" :
-    case   "y" :  
-    alert(` ✔️  Yes, your answer is correct. You have a strong memory 😃 `);
-    console.log(` ${myFavActor} is one of my favourite actor\s` );
-    break;
-    
-    default : 
-    alert("Please answer with only yes/y or no/n with no spaces");
+
+    case "yes":
+    case "y":
+        alert(` ✔️  Yes, your answer is correct. You have a strong memory 😃 `);
+        correctAnswer++;
+        console.log(` ${myFavActor} is one of my favourite actor\s`);
+        break;
+
+    default:
+        alert("Please answer with only yes/y or no/n with no spaces");
 
 }
 
 const myMajor = "Information Technology";
 
-let askForMyMajor=prompt("Do you think that my major is human medicine ? 🤔  chooes Yes or No ");
+let askForMyMajor = prompt("Do you think that my major is human medicine ? 🤔  chooes Yes or No ");
 
-askForMyMajor=askForMyMajor.toLocaleLowerCase();
+askForMyMajor = askForMyMajor.toLocaleLowerCase();
 
-switch(askForMyMajor){
-    case  "yes" :
-    case   "y" :   
+switch (askForMyMajor) {
+    case "yes":
+    case "y":
         alert(` ❌  No , My a major is ${myMajor} 😃 ?? It's okay, let's continue 😃 `);
-        console.log(` My a major is ${myMajor} ` );
+        console.log(` My a major is ${myMajor} `);
         break;
-    
-    case  "no" :
-    case   "n" :  
-    alert(` ✔️  Yes, your answer is correct. My a major is ${myMajor} 😃 `);
-    console.log(` My a major is ${myMajor}` );
-    break;
-    
-    default : 
-    alert("Please answer with only yes/y or no/n with no spaces");
+
+    case "no":
+    case "n":
+        alert(` ✔️  Yes, your answer is correct. My a major is ${myMajor} 😃 `);
+        correctAnswer++;
+        console.log(` My a major is ${myMajor}`);
+        break;
+
+    default:
+        alert("Please answer with only yes/y or no/n with no spaces");
 
 }
 
 
 
-let askForPinion=prompt("Did you enjoy with me 😃 ? chooes Yes or No ");
+let askForPinion = prompt("Did you enjoy with me 😃 ? chooes Yes or No ");
 
-askForPinion=askForPinion.toLocaleLowerCase();
+askForPinion = askForPinion.toLocaleLowerCase();
 
-switch(askForPinion){
-    case  "yes" :
-    case   "y" :   
+switch (askForPinion) {
+    case "yes":
+    case "y":
         alert(`I'm so glad you liked it ${userName} , and I'm going to develop it further. 🥰 `);
+        correctAnswer++;
         console.log(askForPinion);
         break;
-    
-    case  "no" :
-    case   "n" :  
-    alert(` Sorry to hear this ${userName} 😔 , but next time I promise you'll like it. 😃 `);
-    console.log(askForPinion);
-    break;
-    
-    default : 
-    alert("Please answer with only yes/y or no/n with no spaces");
+
+    case "no":
+    case "n":
+        alert(` Sorry to hear this ${userName} 😔 , but next time I promise you'll like it. 😃 `);
+        console.log(askForPinion);
+        break;
+
+    default:
+        alert("Please answer with only yes/y or no/n with no spaces");
 
 }
 
 
-alert(`Welcome again ${userName} 😃 , see you later. 👋 ` );
+let currectNum = 50;
+
+let insertNum ;
+
+for (let i = 0; i < 4; i++) {
+
+    
+     insertNum = prompt("gussing number between 0 to 100 ");
+    if (insertNum == 50) {
+
+        alert(` Good job the currect number is ${currectNum} 😃 `);
+        break;
+
+
+    }
+
+
+    else if (insertNum > currectNum) {
+        alert("try smaller than is number ");
+
+        //insertNum = prompt("guss agian");
+
+
+    }
+    else if (insertNum < currectNum) {
+        alert("try larger this number ");
+
+       // insertNum = prompt("guss agian");
+
+    }
+    else {
+        alert("the currect ansowr 50 !");
+
+
+    }
+
+}
+
+
+correctAnswer++;
+
+
+
+
+
+let myArr = ["pizza", "burger", "pasta", "mansaf"];
+
+let exit_loops = false;
+let clintGuess;
+for (let i = 0; i < 6; i++) {
+
+    clintGuess = prompt("guess my fav food ");
+
+
+    for (let j = 0; j < 4; j++) {
+
+        if (clintGuess === myArr[j]) {
+            let favChooes= myArr[j];
+            alert(`you are amazing the  ${favChooes} one of my fav food `);
+            correctAnswer++;
+            exit_loops = true;
+            break;
+        }
+
+
+
+        
+    }if (exit_loops)
+    break;
+
+    
+    
+    //alert("try agian");
+    clintGuess = prompt("Try agian");
+
+
+}
+
+alert(`Welcome again ${userName} 😃 , You'r scor is ${correctAnswer} of 7 , see you later. 👋 `);
+
+
